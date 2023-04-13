d11f4679baf1ba0b892ddfa2edc803cab1fb8464
SomeoneEtc <someoneetc@yandex.com>
2023-04-13 14:43:34 +0200

    fix

--- main/main/views.py
+++ main/main/views.py
@@ -5,7 +5,7 @@ def serveFile(request):
     #return HttpResponse(response)
     from django.utils.encoding import smart_str
 
-    response = HttpResponse(mimetype='application/force-download') # mimetype is replaced by content_type for django 1.7
+    response = HttpResponse(content_type='application/force-download') # mimetype is replaced by content_type for django 1.7
     response['Content-Disposition'] = 'attachment; filename=%s' % smart_str('prova.txt')
     response['X-Sendfile'] = smart_str('static/prova.txt')
     # It's usually a good idea to set the 'Content-Length' header too.
