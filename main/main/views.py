d4f0fc8c7b2fd0441aabeb9c8651957b7ea95e4a
SomeoneEtc <someoneetc@yandex.com>
2023-04-13 14:37:20 +0200

    prova

--- main/main/views.py
+++ main/main/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 
 def serveFile(request):
-    response = "You're looking at the results of question."
-    return HttpResponse(response)
+    #response = "You're looking at the results of question."
+    #return HttpResponse(response)
+    from django.utils.encoding import smart_str
+
+    response = HttpResponse(mimetype='application/force-download') # mimetype is replaced by content_type for django 1.7
+    response['Content-Disposition'] = 'attachment; filename=%s' % smart_str('provaqr')
+    response['X-Sendfile'] = smart_str('static/prova.txt')
+    # It's usually a good idea to set the 'Content-Length' header too.
+    # You can also set any other required headers: Cache-Control, etc.
+    return response
